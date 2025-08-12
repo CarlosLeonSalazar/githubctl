0259cac495a08b20bb8b641d1b28b6f28b5da129
CarlosLeonSalazar <165884029+CarlosLeonSalazar@users.noreply.github.com>
2025-08-12 13:24:48 +0200

    Edición de main - output param.

--- src/githubctl/main.py
+++ src/githubctl/main.py
@@ -1,5 +1,7 @@
 import os
+from enum import Enum
 import typer
+from typing_extensions import Annotated
 from dotenv import load_dotenv
 from rich import print
 from github import get_all_user_repositories
@@ -16,19 +18,21 @@ repo_app = typer.Typer()
 # Añadir la instancia repo_app a app como "repo"
 app.add_typer(repo_app, name="repo")
 
+class OutputOption(str, Enum):
+    json = "json"
+    csv = "csv"
+    table = "table"
+
+
 # Crear un comando de repo_app con alias "list"
 @repo_app.command(name="list", help="list user repository")
-def list_repos(user : str = typer.Option(..., '--user', "-u", help="github user name")):
-    """Función para listar los repositorios de un usuario
-    
-    Args:
-        user:
-            - str
-            - required"""
+def list_repos(user : Annotated [str, typer.Option(..., '--user', "-u", help="github user name")],
+               output : Annotated [OutputOption, typer.Option('--output', '-o', help='output format: json, csv, table')] = "json"):
     
     # Llamada a función para obtener todos los repositrorios
-    repo = get_all_user_repositories(username=user)
-    print(repo)
+    # repo = get_all_user_repositories(username=user)
+    # print(repo)
+    print(output)
 
 
 if __name__=="__main__":
